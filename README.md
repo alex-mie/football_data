b216e38e697a8f6cf68fae5976747e2b830817bf
alex <614647621@qq.com>
2025-03-06 14:29:59 +0800

    第一次提交

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # football_data
+## 前言
+
 获取足球信息,数据来自于直播吧
+
+## 环境
+
+百度nvm工具 配置好node环境和npm环境  node版本是v10.16.0
+
+## 依赖
+
+在根目录下执行，下载项目依赖
+
+```
+npm install
+```
+
+## 运行
+
+在工程根目录下 运行node app_start.js 即可
+
+## 配置
+
+在basic.json文件配置所需要的联赛名字和联赛日期
+
+```
+{
+    "year":2024,
+    "search_month_start":5,
+    "search_month_end":6,
+    "start_day":1,
+    "end_day":28,
+    "filter": ["J联赛","K联赛","中超"]
+}
+```
+
+## 数据
+
+最终数据存入了excel文件 并生成了临时的.json文件
+
+![image](https://github.com/rm-f1024/football_data/assets/57568495/0010aecc-21d4-4b8e-80a8-f51ba44fa459)
+
+
---
+++ app_start.js
@@ -0,0 +1,72 @@
+const {to_contact_url} = require("./to_get_match_write_json.js");
+const {get_more_info_by_mactchid,to_write_json} = require("./core.js");
+const {write_in_excel,to_make_json,to_get_team_info_in_league} = require("./write_to_excel.js");
+let { search_month_start,search_month_end    } = require("./basic.json")
+
+
+const fs = require('fs');
+const path = require('path');
+
+// 获取当前目录路径
+const directoryPath = './';
+
+// 读取当前目录中的文件
+fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+        console.error('Error reading directory!', err);
+        return;
+    }
+
+    // 遍历找到匹配的文件并删除
+    files.forEach(file => {
+        // 使用正则表达式匹配 xxxx-xx.json 格式的文件名
+        if (/^\d{4}-\d{2}\.json$/.test(file)) {
+            // 构造文件路径
+            const filePath = path.join(directoryPath, file);
+
+            // 删除文件
+            fs.unlink(filePath, err => {
+                if (err) {
+                    console.error(`Error deleting file ${file}`, err);
+                } else {
+                    console.log(`Deleted file: ${file}`);
+                }
+            });
+        }
+    });
+});
+
+
+const get_table = async ()=>{
+    let search_month   =search_month_start
+    for (;search_month  <= search_month_end;search_month++){
+        let start = Date.now()
+        const match_table = await to_contact_url(search_month);
+        // console.log('match_table===============>',match_table)
+         try{
+            const res = await   get_more_info_by_mactchid(match_table)
+            // console.log('res===============>',res)
+            if(res.length != 0 ){
+                const [{日期1:date1}]= res 
+                const date_str   = date1.substring(0,7)
+                // console.log('date_str2222222222222===============>',date_str)
+                to_write_json(res,date_str)
+                let end = Date.now()
+                let diff =(  end - start )/1000
+                console.log(date_str,'共记录场次 ===============>', res.length ,"耗时:",diff,"秒")
+            }
+            write_in_excel();
+            to_get_team_info_in_league();
+         }
+        catch(err){
+            console.log('get_more_info_by_mactchid_err===============>',err)
+        }
+    }
+  
+
+}
+to_make_json();
+get_table();
+
+// get_more_info_by_mactchid();
+
